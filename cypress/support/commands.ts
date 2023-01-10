20e194ca79bf61662b8f345fa078b0231998e08a
Nikita Radzkou <nikita.radzkou@gmail.com>
2023-01-10 19:06:20 +0300

    Add custom command stepInfo()

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -8,18 +8,24 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
+// Adds a command to add steps for tests.
+// If there is an error in the test, writes the last passed step to the array
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('stepInfo', (message: string) => {
+  let arr = Cypress.env('stepInfo') || [];
+  // Add only last step
+  if (arr.length >= 1) {
+    arr = [];
+  }
+  arr.push(message);
+  Cypress.log({
+    displayName: 'StepInfo',
+    message: `${message}`,
+    consoleProps: () => {
+      return {
+        Step: `${message}`
+      };
+    }
+  });
+  Cypress.env('stepInfo', arr);
+});
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,3 +4,15 @@ import 'cypress-localstorage-commands';
 import 'cypress-xpath';
 import 'cypress-iframe';
 import 'cypress-real-events/support';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Description of step which will describe code below.
+       * @param message
+       */
+      stepInfo(message: string): void;
+    }
+  }
+}
