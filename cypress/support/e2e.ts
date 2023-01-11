847a93874c55481ceeab227c92e80a6fa5f6d81f
Nikita Radzkou <nikita.radzkou@gmail.com>
2023-01-11 13:08:32 +0300

    Update e2e file and remove extra

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,6 +4,17 @@ import 'cypress-localstorage-commands';
 import 'cypress-xpath';
 import 'cypress-iframe';
 import 'cypress-real-events/support';
+import { evalUrl } from '../utils/env.utils';
+
+Cypress.on('uncaught:exception', () => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
+beforeEach(() => {
+  Cypress.config().baseUrl = evalUrl(Cypress.env(), true);
+});
 
 declare global {
   namespace Cypress {
